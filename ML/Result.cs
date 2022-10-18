0dc52214f540b9185e5a379c10ba2bfecce6e538
abrah <abrah@DESKTOP-AVT6ORL>
2022-10-18 09:47:29 -0500

    Agregar archivos de proyecto.

---
+++ ML/Result.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ML
+{
+    public class Result
+    {
+        public bool Correct { get; set; }
+        public Object Object { get; set; }
+        public List<object> Objects { get; set; }
+        public string ErrorMessage { get; set; }
+        public Exception exception { get; set; }
+    }
+}
