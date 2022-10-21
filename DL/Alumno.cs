9c994afd44fe5fb647da8136375afa63105608bc
AbrahamRJ04 <abrahammjimenez@hotmail.com>
2022-10-20 20:21:43 -0500

    Modificacion a DL Alumno

--- DL/Alumno.cs
+++ DL/Alumno.cs
@@ -20,6 +20,30 @@ namespace DL
         public decimal? Promedio { get; set; }
         public int? IdServicioSocial { get; set; }
 
+        //*ESTAS SON PROPIEDADES MANIPULADAS APARTE DE ENTITYUFRAMEWORK*//
+
+        public string NombreCarrera { get; set; }
+        public string NombreDependencia { get; set; }
+        public int IdTipoDependencia { get; set; }
+        public string TipoDeDependencia { get; set; }
+        public string NombreDepartamento { get; set; }
+        public string Domicilio { get; set; }
+        public string Municipio { get; set; }
+        public string TelefonoDependencia { get; set; }
+        public string ResponsableNombre { get; set; }
+        public string CargoResponsable { get; set; }
+        public int IdPrograma { get; set; }
+        public string NombrePrograma { get; set; }
+        public string Actividades { get; set; }
+        public int HorasAlDia { get; set; }
+        public string Turno { get; set; }
+        public DateTime FechaInicio { get; set; }
+        public DateTime FechaTermino { get; set; }
+        public int IdTipoServicio { get; set; }
+        public string TipoDeServicio { get; set; }
+
+        //*ESTAS SON PROPIEDADES MANIPULADAS APARTE DE ENTITYUFRAMEWORK*//
+
         public virtual Carrera? IdCarreraNavigation { get; set; }
         public virtual ServicioSocial? IdServicioSocialNavigation { get; set; }
     }
